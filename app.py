2f598747cc098ee576de62f498cb086bf3211728
Manon Champredon <mah-non@hotmail.com>
2023-06-02 14:27:21 +0800

    query

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ if st.button('Predict'):
                    "passenger_count":passenger_count}
 
     #https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
-    response = requests.get(url,params=querystring)
+    response = requests.get(url,querystring)
     st.write(response.json()["detail"])
