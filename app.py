9e8b09374cc002f04ff55d7683cbdc816045966e
Manon Champredon <mah-non@hotmail.com>
2023-06-02 15:45:13 +0800

    new

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ passenger_count = st.number_input('passenger_count', value =1)
 
 
 
-if st.button('Predict'):
-    url = "https://taxifare.lewagon.ai/predict"
+#if st.button('Predict'):
+    #url = "https://taxifare.lewagon.ai/predict"
 
-    querystring = {"pickup_datetime":pickup_datetime,
-                   "pickup_longitude":pickup_longitude,
-                   "pickup_latitude":pickup_latitude,
-                   "dropoff_longitude":dropoff_longitude,
-                   "dropoff_latitude":dropoff_latitude,
-                   "passenger_count":passenger_count}
+    #querystring = {"pickup_datetime":pickup_datetime,
+                  # "pickup_longitude":pickup_longitude,
+                   #"pickup_latitude":pickup_latitude,
+                   #"dropoff_longitude":dropoff_longitude,
+                   #"dropoff_latitude":dropoff_latitude,
+                  #"passenger_count":passenger_count}
 
     #https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
     #response = requests.get(url, params = querystring)
